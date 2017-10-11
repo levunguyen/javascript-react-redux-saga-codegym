b7da66f12b3ebc01d51614516faa5e75eeaad425
levunguyen <levunguyendn@gmail.com>
2017-10-11 13:47:39 +0700

    fix saga

--- src/sagas/watcher.js
+++ src/sagas/watcher.js
@@ -1,8 +1,7 @@
-import { takeLatest } from 'redux-saga/effects';
-import { searchMediaSaga } from './mediaSaga';
+import { takeLatest } from 'redux-saga';
+import searchMediaSaga from './mediaSagas';
 import * as types from '../constants/actionTypes';
 
-// Watches for SEARCH_MEDIA_REQUEST action type asynchronously
 export default function* watchSearchMedia() {
-    yield takeLatest(types.SEARCH_MEDIA_REQUEST, searchMediaSaga);
+    yield* takeLatest(types.SEARCH_MEDIA_REQUEST, searchMediaSaga);
 }
