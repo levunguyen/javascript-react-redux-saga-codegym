a60fb6170c58f3e62d719431119c0f18d40a374f
levunguyen <levunguyendn@gmail.com>
2017-10-16 15:46:06 +0700

    news page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router';
 import BannerVideo from './BannerVideo'
-import News from './News';
+import NewsPage from '../containers/NewsPage';
 import Programmings from './Programmings';
 import Gallery from './Gallery'
 // Home page component. This serves as the welcome page with link to the library
 const HomePage = () => (
     <section>
         <BannerVideo />
-        <News />
+        <NewsPage />
         <Programmings />
         <Gallery />
 
---
+++ src/containers/NewsPage.js
@@ -0,0 +1,22 @@
+import React, { PropTypes, Component } from 'react';
+import News from "../components/News";
+
+
+export default class NewsPage extends Component {
+
+
+    constructor() {
+        super();
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <News />
+        );
+    }
+
+}
